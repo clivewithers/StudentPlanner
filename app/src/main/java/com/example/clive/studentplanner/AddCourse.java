8ed0577fa4c7600740ef4e20be71d85586dd8031
Clive <cwithers73@gmail.com>
2017-06-03 03:23:38 +1200

    dropboxes now populate in add course class

--- app/src/main/java/com/example/clive/studentplanner/AddCourse.java
+++ app/src/main/java/com/example/clive/studentplanner/AddCourse.java
@@ -1,22 +1,74 @@
 package com.example.clive.studentplanner;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
-public class AddCourse extends AppCompatActivity {
+public class AddCourse extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private String weekday;
+    private String course;
+    private String startTime;
+    private String finishTime;
+    private String room;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_course);
+
+        Spinner spnPaperLevel = (Spinner) findViewById(R.id.spnLevelPaper);
+        final Spinner spnCourseId = (Spinner) findViewById(R.id.spnCourseId);
+        Spinner spnWeekdays = (Spinner) findViewById(R.id.spn_DayOfWeek);
+
+        ArrayAdapter adaptorPaperLevel = ArrayAdapter.createFromResource(this, R.array.level, android.R.layout.simple_spinner_item);
+        spnPaperLevel.setAdapter(adaptorPaperLevel);
+        spnPaperLevel.setOnItemSelectedListener(this);
+
+        final ArrayAdapter adaptorCourse5 = ArrayAdapter.createFromResource(this, R.array.Level5, android.R.layout.simple_spinner_item);
+        final ArrayAdapter adaptorCourse6 = ArrayAdapter.createFromResource(this, R.array.level6, android.R.layout.simple_spinner_item);
+        final ArrayAdapter adaptorCourse7 = ArrayAdapter.createFromResource(this, R.array.level7, android.R.layout.simple_spinner_item);
+
+        spnCourseId.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item));
+
+        do {
+                spnPaperLevel.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                        if (position == 1){
+                            spnCourseId.setAdapter(adaptorCourse5);
+                            spnCourseId.setOnItemSelectedListener(this);
+                        }else if (position == 2){
+                            spnCourseId.setAdapter(adaptorCourse6);
+                            spnCourseId.setOnItemSelectedListener(this);
+                        }else if (position == 3){
+                            spnCourseId.setAdapter(adaptorCourse7);
+                            spnCourseId.setOnItemSelectedListener(this);
+                        }
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> adapterView) {
+                        return;
+                    }
+                });
+        }
+        while (spnCourseId.getSelectedItemPosition() == 0);
+
+        ArrayAdapter adaptor = ArrayAdapter.createFromResource(this, R.array.weekdays, android.R.layout.simple_spinner_item);
+        spnWeekdays.setAdapter(adaptor);
+        spnWeekdays.setOnItemSelectedListener(this);
 
         if (getSupportActionBar() != null) {
             getSupportActionBar().setTitle("Add Study Times");
@@ -72,5 +124,15 @@ public class AddCourse extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        weekday = (String) view.toString().trim();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
